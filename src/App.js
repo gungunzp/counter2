07e90be61a23dd6be1f30b83a6be6038eb7ec9a1
gungunzp <flavazp@gmail.com>
2022-07-01 16:52:51 +0300

    add fonts, font-awesome and markup from counter1 repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import './style.scss';
 
 function App() {
-	return <div className='test'>Counter 2</div>;
+	return (
+		<main className="wrapper">
+			<section className="category">
+				<h2 className="category__title">Відмовились:</h2>
+				<div id="declined" className="category__actions">
+					<button>-</button>
+					<span className="category__value">0</span>
+					<button>+</button>
+				</div>
+			</section>
+			<section className="category">
+				<h2 className="category__title">Відповіли на питання:</h2>
+				<div id="answered" className="category__actions">
+					<button>-</button>
+					<span className="category__value">0</span>
+					<button>+</button>
+				</div>
+			</section>
+			<section className="category">
+				<h2 className="category__title">Почули Євангеліє:</h2>
+				<div id="listened" className="category__actions">
+					<button>-</button>
+					<span className="category__value">0</span>
+					<button>+</button>
+				</div>
+			</section>
+			<section className="category">
+				<h2 className="category__title">Усього:</h2>
+				<div id="total" className="category__actions category__actions--total">
+					<button id="reset">
+						<i className="fa-solid fa-arrow-rotate-right"></i>
+					</button>
+					<span className="category__value">0</span>
+					<button id="share">
+						<i className="fa-solid fa-arrow-up-right-from-square"></i>
+					</button>
+				</div>
+			</section>
+		</main>
+	);
 }
 
 export default App;
